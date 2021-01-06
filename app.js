e5f4e2ab5dbe963018d46ae7ea76d08b0c66aefc
isaque <isaque.financas@gmail.com>
2021-01-06 00:03:05 -0300

    second comit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const { nome } = req.query;
+  return res.json({
+    pagina: 'HOME',
+    nome,
+  })
+})
+
+app.get('/blog', (req, res) => {
+  const { nome } = req.query;
+  return res.json({
+    pagina: 'BLOG',
+    nome,
+  })
+})
+
+app.get('/produtos', (req, res) => {
+  const { nome } = req.query;
+  return res.json({
+    pagina: 'PRODUTOS',
+    nome,
+  })
+})
+
+app.listen(3333);
